4f3a9bed874e40f504936c37f3fc88281ab508c3
HeavyMaverick <114074204+HeavyMaverick@users.noreply.github.com>
2024-07-09 19:26:47 +0700

    up

--- SpringProj/src/main/java/com/heavymaverick/hibernate_test/Test2.java
+++ SpringProj/src/main/java/com/heavymaverick/hibernate_test/Test2.java
@@ -5,25 +5,22 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-import java.util.List;
-
 public class Test2 {
     public static void main(String[] args) {
         try (SessionFactory sessionFactory = new Configuration()
                 .configure("hibernate.cfg.xml")
                 .addAnnotatedClass(Employee.class)
-                .buildSessionFactory()) {
+                .buildSessionFactory()){
+
             Session session = sessionFactory.getCurrentSession();
             session.beginTransaction();
-
-            List employees = session.createQuery("from Employee where name = 'Elena' AND salary > 650").getResultList();
-            for (Object employee : employees) {
-                System.out.println(employee);
-            }
-
+//            Employee emp = session.get(Employee.class, 1);
+//            session.delete(emp);
+//            session.createQuery("delete Employee where name = 'Oleg'").executeUpdate();
+            Employee emp = new Employee("AAA", "BBB", "IT", 750);
+            session.save(emp);
             session.getTransaction().commit();
-            System.out.println("!DONE!");
-
+            session.close();
         }
     }
 }
