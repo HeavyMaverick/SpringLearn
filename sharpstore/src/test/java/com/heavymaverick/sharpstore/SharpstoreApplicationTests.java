ee553c09076fa9d09ac1efe33573e9da37b625c3
HeavyMaverick <114074204+HeavyMaverick@users.noreply.github.com>
2024-07-09 20:22:20 +0700

    up
    started proj

---
+++ sharpstore/src/test/java/com/heavymaverick/sharpstore/SharpstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heavymaverick.sharpstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharpstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
