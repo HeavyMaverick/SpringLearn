79439793aff2ef89270c4a0709820cfdce45c8c2
HeavyMaverick <114074204+HeavyMaverick@users.noreply.github.com>
2024-08-02 17:17:47 +0700

    up

--- WebProj/catalogue-service/src/main/java/com/heavymaverick/catalogue/controller/ProductsRestController.java
+++ WebProj/catalogue-service/src/main/java/com/heavymaverick/catalogue/controller/ProductsRestController.java
@@ -10,6 +10,7 @@ import org.springframework.context.MessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -24,7 +25,6 @@ import java.util.Map;
 public class ProductsRestController {
 
     private final ProductService productService;
-    private  final MessageSource messageSource;
 
     @GetMapping
     public List<Product> findProducts() {
@@ -34,16 +34,13 @@ public class ProductsRestController {
     @PostMapping()
     public ResponseEntity<?> createProduct(@Valid @RequestBody NewProductPayload payload,
                                                  BindingResult bindingResult,
-                                                 UriComponentsBuilder uriBuilder, Locale locale) {
+                                                 UriComponentsBuilder uriBuilder) throws BindException {
         if (bindingResult.hasErrors()) {
-            ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST,
-                    this.messageSource.getMessage("errors.400.title", new Object[0], "errors.400.title", locale));
-            problemDetail.setProperty("errors",
-                    bindingResult.getAllErrors().stream()
-                            .map(MessageSourceResolvable::getDefaultMessage)
-                            .toList());
-            return ResponseEntity.badRequest()
-                    .body(problemDetail);
+            if (bindingResult instanceof BindException exception) {
+                throw exception;
+            } else {
+                throw new BindException(bindingResult);
+            }
         } else {
             Product product = this.productService.createProduct(payload.title(), payload.details());
             return ResponseEntity
