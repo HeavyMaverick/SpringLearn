786cc6cb66b00e9f17b831caf1f4ddb4badbd802
HeavyMaverick <114074204+HeavyMaverick@users.noreply.github.com>
2024-11-30 16:43:40 +0700

    ready

--- Organiser/src/main/java/com/heavymaverick/OrganiserApplication.java
+++ Organiser/src/main/java/com/heavymaverick/OrganiserApplication.java
@@ -2,11 +2,26 @@ package com.heavymaverick;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
 
 @SpringBootApplication
 public class OrganiserApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(OrganiserApplication.class, args);
+    }
+
+    @SuppressWarnings("removal")
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf().disable()
+                .httpBasic().and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+                .authorizeHttpRequests().anyRequest().authenticated().and()
+                .build();
     }
 }
--- Organiser/src/test/java/com/heavymaverick/TasksRestControllerTest.java
+++ Organiser/src/test/java/com/heavymaverick/TasksRestControllerTest.java
@@ -1,12 +1,10 @@
 package com.heavymaverick;
 
-import org.apache.logging.log4j.message.Message;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.context.MessageSource;
 import org.springframework.http.HttpStatus;
@@ -37,11 +35,13 @@ class TasksRestControllerTest {
     @DisplayName("GET /api/tasks возвращает HTTP-ответ со статусом 200 OK и списком задач")
     void handleGetAllTasksTest_ReturnsValidResponseEntity() {
         // given
-        var tasks = List.of(new Task(UUID.randomUUID(), "Первая задача", false),
-                new Task(UUID.randomUUID(), "Вторая задача", true));
-        doReturn(tasks).when(taskRepository).findAll();
+        var user = new ApplicationUser(UUID.randomUUID(), "user1", "password1");
+
+        var tasks = List.of(new Task(UUID.randomUUID(), "Первая задача", false, user.id()),
+                new Task(UUID.randomUUID(), "Вторая задача", true, user.id()));
+        doReturn(tasks).when(taskRepository).findByApplicationUserId(user.id());
         // when
-        var responseEntity = controller.handleGetAllTasks();
+        var responseEntity = controller.handleGetAllTasks(user);
 
         // then
         assertNotNull(responseEntity);
@@ -53,9 +53,10 @@ class TasksRestControllerTest {
     @Test
     void handleCreateNewTask_PayloadIsValid_ReturnsValidResponseEntity() {
         // given
+        var user = new ApplicationUser(UUID.randomUUID(), "user1", "password1");
         var details = "Третья задача";
         // when
-        var responseEntity = controller.handleCreateNewTask(new NewTaskPayload(details),
+        var responseEntity = controller.handleCreateNewTask(user, new NewTaskPayload(details),
                 UriComponentsBuilder.fromUriString("http://localhost:8080"), Locale.ENGLISH);
 
         // then
@@ -66,6 +67,7 @@ class TasksRestControllerTest {
             assertNotNull(task.id());
             assertEquals(details, task.details());
             assertFalse(task.completed());
+            assertEquals(user.id() ,task.applicationUserId());
 
             assertEquals(URI.create("http://localhost:8080/api/tasks/" + task.id()),
                     responseEntity.getHeaders().getLocation());
@@ -80,6 +82,7 @@ class TasksRestControllerTest {
     @Test
     void handleCreateNewTask_PayloadIsInvalid_ReturnsValidResponseEntity() {
         // given
+        var user = new ApplicationUser(UUID.randomUUID(), "user1", "password1");
         var details = "   ";
         var locale = Locale.US;
         var errorMessage = "Details is empty";
@@ -87,7 +90,7 @@ class TasksRestControllerTest {
                 .getMessage("tasks.create.details.errors.not_set", new Object[0], locale);
 
         //when
-        var responseEntity = controller.handleCreateNewTask(new NewTaskPayload(details),
+        var responseEntity = controller.handleCreateNewTask(user, new NewTaskPayload(details),
                 UriComponentsBuilder.fromUriString("http://localhost:8080"), locale);
 
         // then
